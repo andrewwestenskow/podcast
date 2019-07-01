225baf549d471caf4fda2e7a78701a480de89b6a
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-07-01 13:45:37 -0600

    Fixed some styling

--- src/Components/Admin/AddSpecialEpisode.jsx
+++ src/Components/Admin/AddSpecialEpisode.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import AuthHeader from './AuthHeader'
 import axios from 'axios'
+import ReactQuill from 'react-quill'
+import {modules} from '../../Assets/quillModules'
 
 class AddSpecialEpisode extends Component {
 
@@ -9,7 +11,11 @@ class AddSpecialEpisode extends Component {
     title: '',
     summary: '',
     poster: '',
-    backdrop: ''
+    backdrop: '',
+    movies: [],
+    movieSearch: '',
+    showResult: false,
+    results: []
   }
 
   handleChange = (e) => {
@@ -35,6 +41,25 @@ class AddSpecialEpisode extends Component {
     }
   }
 
+  handleReviewChange = (value) => {
+    this.setState({ summary: value })
+  }
+
+  searchMovies = async (e) => {
+    e.preventDefault()
+    this.setState({
+      searching: true
+    })
+    let searchString = this.state.movieSearch.split(' ').join('+')
+    let results = await axios.get(`/movies?searchString=${searchString}`)
+    this.setState({
+      results: results.data.results,
+      movieTitle: '',
+      showResults: true,
+      searching: false
+    })
+  }
+
   render() {
     return (
       <>
@@ -49,7 +74,7 @@ class AddSpecialEpisode extends Component {
               <p>Episode Backdrop</p>
               <input onChange={(e) => this.handleChange(e)} type="text" name="backdrop" />
               <p>Summary</p>
-              <input onChange={(e) => this.handleChange(e)} type="text" name='synopsis' />
+              <ReactQuill modules={modules} onChange={this.handleSummaryChange} value={this.state.summary} className='number-quill' theme='snow' />
               <button type='submit'>Create episode</button>
             </form>
           </section>
@@ -58,6 +83,14 @@ class AddSpecialEpisode extends Component {
             <img src={this.state.poster} className='preview-poster' alt=""/>
             <p>Backdrop</p>
             <img src={this.state.backdrop} className='preview-backdrop' alt=""/>
+            <p>Movie search</p>
+            <input onChange={(e)=>this.handleChange(e)} type="text" name='movieSearch' value={this.state.movieSearch}/>
+            {!this.state.showResult ? <></> : 
+          <div className='special-results-hold'>
+            <form onSubmit={(e) => this.addToSpecial(e)}>
+              {/* <img src={} alt=""/> */}
+            </form>
+          </div>}
           </section>
           <section className="column">
             Confirm your details
