bdb57144c995acefe469012e4db3d64117bfbae1
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-07-02 14:48:00 -0600

    Blog setup

--- src/Components/Blog/BlogListHold.jsx
+++ src/Components/Blog/BlogListHold.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class BlogListHold extends Component {
+
+  state = {
+    blogs: [],
+    loading: true
+  }
+
+  async componentDidMount() {
+    let blogs = await axios.get('/api/blogs')
+    this.setState({
+      blogs: blogs.data,
+      loading: false
+    })
+  }
 
 
   render() {
 
     return (
       <div className="EpisodeList">
-        <div className="episode-list-hold">
-          <h1>Blog Posts:</h1>
-        </div>
+        {this.state.loading ? <div>Loading...</div> :
+          <div className="episode-list-hold">
+            <h1>Blog Posts:</h1>
+          </div>
+        }
       </div>
     )
   }
